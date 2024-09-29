d8544d2ca76fd7222d469dca07b73014f8e090de
alan <asilvar4@correo.uss.cl>
2024-09-28 22:06:55 -0300

    update user.html

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template, g
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('./index.html')
 
 @app.route('/login')
 def login():
     return render_template('./login.html')
+
+@app.route('/user')
+def user():
+    return render_template('./user.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
